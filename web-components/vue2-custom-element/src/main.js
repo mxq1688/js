e2e3ead72a8358074004bcfa332c36c2006115b5
孟献强 <xianqiang.meng@mobvoi.com>
2022-02-18 14:35:19 +0800

    fix:web component update

--- web-components/vue2-custom-element/src/main.js
+++ web-components/vue2-custom-element/src/main.js
@@ -23,14 +23,17 @@ Vue.use(vueCustomElement)
 Vue.use(AtUI)
 Vue.config.productionTip = false
 
-// new Vue is not needed anymore, as you're initializing the app through vue-custom-element
-// new Vue({
-//   router,
-//   store,
-//   render: h => h(App)
-// }).$mount('#app')
 
 // use vue-custom-element
 App.store = store
 App.router = router
 Vue.customElement('vue-widget', App)
+
+// new Vue is not needed anymore, as you're initializing the app through vue-custom-element
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+
