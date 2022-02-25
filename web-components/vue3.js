136ac5a5e309d2da200962265143a4ae391fa714
孟献强 <xianqiang.meng@mobvoi.com>
2022-02-25 17:03:09 +0800

    fix:vue3 component

--- web-components/vue3.js
+++ web-components/vue3.js
@@ -1,11 +1,30 @@
-import { defineCustomElement, ref, h } from 'vue'
+import { defineCustomElement, ref, h } from "vue";
 const MyVueElement = defineCustomElement({
-    setup () {
-      const count = ref(0)
-      return () => h('div', {
-        onClick: () => count.value++
-      }, `点击${count.value}`)
-    }
-  })
-  
-  customElements.define('my-vue-element', MyVueElement)
+  setup() {
+    const count = ref(0);
+    return () =>
+      h(
+        "div",
+        {
+          onClick: () => count.value++,
+        },
+        `点击${count.value}`
+      );
+  },
+});
+
+customElements.define("my-vue-element", MyVueElement);
+
+const Test = {
+  setup() {
+    const inst = getCurrentInstance();
+    console.log("inst", inst);
+
+    Object.assign(inst.appContext, app._context);
+    console.log(inst.appContext, App);
+
+    return () => h(App);
+  },
+};
+
+customElements.define("my-vue-element", defineCustomElement(Test));
