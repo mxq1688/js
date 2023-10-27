89f3a0aacdd2ddee21758c3c3d6a0a914032c274
xianqiang.meng <xianqiang.meng@mobvoi.com>
2023-10-27 17:45:06 +0800

    update

---
+++ Ts-Node/express.js
@@ -0,0 +1,36 @@
+const http = require('http')
+function express(){
+    var funcs = [];
+
+    var expr = function(req,res){
+        var i = 0;
+        function next(){            
+            var task = funcs[i++];
+            if(!task) return;
+            task(req,res,next);
+        }
+        next();
+    }
+    //use是expr函数的静态方法 
+    expr.use=function(f){
+        funcs.push(f);
+    }
+    return expr;
+}
+var app = express();
+
+app.use(function(req,res,next){
+    console.log('haha');
+    next();
+});
+app.use(function(req,res,next){
+    console.log('hehe');
+    next();
+});
+app.use(function(req,res){
+    res.end("there is nothing happened");
+});
+
+http.createServer(app).listen('3000', function(){
+  console.log('Express server listening on port 3000');
+});
