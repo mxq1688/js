67f30da37731c47b74c2b29c54e5144555c85dcf
mxq <1365732392@qq.com>
2024-01-18 22:37:43 +0800

    update

--- Lib/gulpdemo/gulpfile.copy.js
+++ Lib/gulpdemo/gulpfile.copy.js
@@ -1,7 +1,10 @@
 const { src, dest } = require("gulp");
+// src()和dest()这两个函数在我们实际项目中经常会用到。
+// src()表示创建一个读取文件系统的流，dest()是创建一个写入到文件系统的流
 
 const copy = () => {
-  return src("src/*/*").pipe(dest("dist/"));
+  return src("src/*/*")
+        .pipe(dest("dist/"));
     // src目录里有多级目录，src/*/*
 };
 
---
+++ Lib/gulpdemo/gulpfile.less.js
@@ -0,0 +1,28 @@
+// const { src, dest } = require("gulp");
+// const lessTask = () => {
+//   return src("src/style/*.less")
+//         .pipe(less())
+//         .pipe(dest("dist/style"));
+// };
+// exports.default = lessTask;
+
+
+const { src, dest } = require("gulp");
+const less = require("gulp-less");
+
+const autoprefixer = require("gulp-autoprefixer");
+const lessTask = () => {
+  return src("src/style/*.less")
+    .pipe(less())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["> 1%", "last 2 versions"],
+        cascade: false, //  是否美化属性值
+      })
+    )
+    .pipe(dest("dist/style"));
+};
+
+
+exports.default = lessTask;
+
