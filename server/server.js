6b334725c6353e94c1e3688efde73666860c985e
xianqiang.meng <xianqiang.meng@mobvoi.com>
2024-07-08 18:21:13 +0800

    udpate

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let http = require('http')
+let path = require('path')
 let app = express();
-app.use(express.static('./')) //匹配静态资源 包括index.html
-
+app.use(express.static(path.join(__dirname, './'))) //匹配静态资源 包括index.html
 // app.listen(3000);
 http.createServer(app).listen(3000);
